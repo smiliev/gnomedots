9cd15d792a1aeffe4cb67b32641b29d049284a06
Stilian Iliev <spr1nkler@protonmail.com>
2026-05-03 02:58:55 +0300

    fix dconf backup

--- load_settings.sh
+++ load_settings.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Load the saved settings back into dconf
-dconf load /org/gnome/desktop/wm/keybindings/ < ./dconf-backups/wm-scripts.dconf
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ./dconf-backups/custom-keys.dconf
-dconf load /org/gnome/shell/extensions/forge/ < ./dconf-backups/forge-settings.dconf
-
-echo "Settings have been successfully restored!"
+# This loads everything back to the root of /org/gnome/
+dconf load /org/gnome/ < ./dconf-backups/gnome_all.dconf
+echo "Settings restored."
